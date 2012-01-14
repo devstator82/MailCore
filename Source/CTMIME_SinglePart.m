f6960a152f40e0a33c2582582b9fc237a755030c
Matt Ronge <mronge@mronge.com>
2012-01-14 16:32:21 -0600

    Forget to clear out the progress handler after finishing a fetch

--- Source/CTMIME_SinglePart.m
+++ Source/CTMIME_SinglePart.m
@@ -141,11 +141,13 @@ static void download_progress_callback(size_t current, size_t maximum, void * co
         
         mailimap_set_progress_callback(get_imap_session(mMessage), &download_progress_callback, NULL, block);  
 		r = mailmessage_fetch_section(mMessage, mMime, &fetchedData, &fetchedDataLen);
+        mailimap_set_progress_callback(get_imap_session(mMessage), NULL, NULL, NULL); 
 		if (r != MAIL_NO_ERROR) {
 		    // This is already released if mailmessage_fetch_section fails - this double free would crash us on iOS 4
 			//mailmessage_fetch_result_free(mMessage, fetchedData);
 			RaiseException(CTMIMEParseError, CTMIMEParseErrorDesc);
 		}
+        
 
 		size_t current_index = 0;
 		char * result;
